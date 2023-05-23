2d999c3e105426d4ef2b83897caaec9cf75c0b6c
Shaikh Amaan <113681911+joyboy7867@users.noreply.github.com>
2023-05-23 14:31:36 +0530

    Update1 db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb+srv://zoro:zoro7867!@cluster0.jtd1tm9.mongodb.net/";
+const mongoURI="mongodb+srv://amaankp7867:amaan07%40@cluster0.xx8rf7j.mongodb.net/";
 
 const connecttomongo=()=>{
     mongoose.connect(mongoURI).then(()=>{console.log("connected to database")}).catch(()=>{console.log("not connected")})
